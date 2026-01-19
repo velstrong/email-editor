98ebbca14a5ad2935b87821646321b563b2ac945
velstrong <32228508+velstrong@users.noreply.github.com>
2026-01-19 21:32:55 +0400

    Update WidthHeight.tsx

--- src/Components/Mods/WidthHeight.tsx
+++ src/Components/Mods/WidthHeight.tsx
@@ -1,8 +1,8 @@
 import _ from 'lodash';
 import { useEditor } from '../../Hooks/Editor.hook';
-import { Form, Input } from 'antd';
+import { Box, TextField, Typography } from '@mui/material';
 import { useValue, useVisibility } from '../../Hooks/Attribute.hook';
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const ATTRIBUTE = 'width';
 
@@ -11,7 +11,10 @@ interface WidthHeightProps {
 }
 
 export const Width = ({ activePath }: WidthHeightProps) => {
-  const [visible, path] = useVisibility({ attribute: ATTRIBUTE, customPath: activePath });
+  const [visible, path] = useVisibility({
+    attribute: ATTRIBUTE,
+    customPath: activePath,
+  });
 
   const { mjmlJson, setMjmlJson } = useEditor();
   const [value, setValue] = useState<string>('');
@@ -21,32 +24,46 @@ export const Width = ({ activePath }: WidthHeightProps) => {
     if (visible) {
       setValue(getValue());
     }
-  }, [visible]);
+  }, [visible, getValue]);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value);
-    if (e.target.value === '' || e.target.value === '%') {
-      e.target.value = '0';
+    let newValue = e.target.value;
+    setValue(newValue);
+
+    if (newValue === '' || newValue === '%') {
+      newValue = '0';
     }
-    if (visible && path && e.target.value) {
-      let json = {};
-      let element = _.get(mjmlJson, path);
-      element.attributes[ATTRIBUTE] = e.target.value;
-      json = _.set(mjmlJson, path, element);
+
+    if (visible && path && newValue) {
+      const element = _.get(mjmlJson, path);
+      element.attributes[ATTRIBUTE] = newValue;
+      const json = _.set(mjmlJson, path, element);
       setMjmlJson({ ...json });
     }
   };
+
   const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.currentTarget.value.length === 1 && e.key === 'Backspace') {
       e.currentTarget.value = '';
     }
   };
 
-  return visible ? (
-    <Form.Item label="Width">
-      <Input onChange={handleChange} value={value} onKeyDown={onKeyDown} />
-    </Form.Item>
-  ) : null;
+  if (!visible) return null;
+
+  return (
+    <Box>
+      <Typography variant="subtitle2" gutterBottom>
+        Width
+      </Typography>
+      <TextField
+        fullWidth
+        size="small"
+        value={value}
+        onChange={handleChange}
+        onKeyDown={onKeyDown}
+      />
+    </Box>
+  );
 };
 
 const ATTRIBUTE_HEIGHT = 'height';
@@ -54,37 +71,55 @@ const ATTRIBUTE_HEIGHT = 'height';
 export const Height = () => {
   const [visible, path] = useVisibility({ attribute: ATTRIBUTE_HEIGHT });
   const { mjmlJson, setMjmlJson } = useEditor();
-  const { getValue } = useValue({ path, visible, attribute: ATTRIBUTE_HEIGHT });
+  const { getValue } = useValue({
+    path,
+    visible,
+    attribute: ATTRIBUTE_HEIGHT,
+  });
   const [value, setValue] = useState('');
 
   useEffect(() => {
     if (visible) {
       setValue(getValue());
     }
-  }, [visible]);
+  }, [visible, getValue]);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value);
-    if (e.target.value === '' || e.target.value === '%') {
-      e.target.value = '';
+    let newValue = e.target.value;
+    setValue(newValue);
+
+    if (newValue === '' || newValue === '%') {
+      newValue = '';
     }
-    if (visible && path && e.target.value) {
-      let json = {};
-      let element = _.get(mjmlJson, path);
-      element.attributes[ATTRIBUTE_HEIGHT] = e.target.value;
-      json = _.set(mjmlJson, path, element);
+
+    if (visible && path && newValue) {
+      const element = _.get(mjmlJson, path);
+      element.attributes[ATTRIBUTE_HEIGHT] = newValue;
+      const json = _.set(mjmlJson, path, element);
       setMjmlJson({ ...json });
     }
   };
+
   const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.currentTarget.value.length === 1 && e.key === 'Backspace') {
       e.currentTarget.value = '';
     }
   };
 
-  return visible ? (
-    <Form.Item label="Height">
-      <Input onChange={handleChange} value={value} onKeyDown={onKeyDown} />
-    </Form.Item>
-  ) : null;
+  if (!visible) return null;
+
+  return (
+    <Box>
+      <Typography variant="subtitle2" gutterBottom>
+        Height
+      </Typography>
+      <TextField
+        fullWidth
+        size="small"
+        value={value}
+        onChange={handleChange}
+        onKeyDown={onKeyDown}
+      />
+    </Box>
+  );
 };
