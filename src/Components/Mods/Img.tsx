c581632116c250ae5457fcb413dfb5097441492d
velstrong <32228508+velstrong@users.noreply.github.com>
2026-01-19 21:38:21 +0400

    Update Img.tsx

--- src/Components/Mods/Img.tsx
+++ src/Components/Mods/Img.tsx
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 import { ChangeEvent } from 'react';
 import { useEditor } from '../../Hooks/Editor.hook';
-import { Col, Input, Row } from 'antd';
+import { Grid, TextField, InputAdornment } from '@mui/material';
 import { useValue, useVisibility } from '../../Hooks/Attribute.hook';
 
 const ATTRIBUTE = 'src';
@@ -11,27 +11,39 @@ const Img = () => {
   const { mjmlJson, setMjmlJson } = useEditor();
 
   const { getValue } = useValue({ path, visible, attribute: ATTRIBUTE });
-  //todo: check if changing directly to mjmlJson causes perfomance impact
-  //   if so, then maintain a local state, then on change change the value first,
-  //   then by using useeffect listen for the in localvalue, then update the mjmlJson.
+
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    let value = e.currentTarget.value;
+    const value = e.currentTarget.value;
+
     if (path && visible) {
-      let json = {};
-      let element = _.get(mjmlJson, path);
+      const element = _.get(mjmlJson, path);
       element.attributes[ATTRIBUTE] = value;
-      json = _.set(mjmlJson, path, element);
+      const json = _.set(mjmlJson, path, element);
       setMjmlJson({ ...json });
     }
   };
 
-  return visible ? (
-    <Row>
-      <Col span={24}>
-        <Input addonBefore="src" onChange={(e) => handleChange(e)} value={getValue()} />
-      </Col>
-    </Row>
-  ) : null;
+  if (!visible) return null;
+
+  return (
+    <Grid container>
+      <Grid item xs={12}>
+        <TextField
+          fullWidth
+          size="small"
+          value={getValue()}
+          onChange={handleChange}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                src
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Grid>
+    </Grid>
+  );
 };
 
 export { Img };
