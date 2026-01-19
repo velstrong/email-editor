3b7e3ab5ceb95ed9f32d37fe3446960e73a177e6
velstrong <32228508+velstrong@users.noreply.github.com>
2026-01-19 21:36:49 +0400

    Update InnerPadding.tsx

--- src/Components/Mods/InnerPadding.tsx
+++ src/Components/Mods/InnerPadding.tsx
@@ -1,6 +1,6 @@
-import { Form, Input } from 'antd';
 import _ from 'lodash';
 import { ChangeEvent, useEffect, useState } from 'react';
+import { Box, TextField, Typography } from '@mui/material';
 import { useVisibility } from '../../Hooks/Attribute.hook';
 import { useEditor } from '../../Hooks/Editor.hook';
 import { logger } from '../../Utils/logger';
@@ -9,36 +9,56 @@ import { UpdateValue } from '../../Utils/operations';
 const ATTRIBUTE = 'inner-padding';
 
 const InnerPadding = () => {
-  let [visible, path] = useVisibility({ attribute: ATTRIBUTE });
+  const [visible, path] = useVisibility({ attribute: ATTRIBUTE });
   const { mjmlJson, setMjmlJson } = useEditor();
   const [value, setValue] = useState<string>();
 
   useEffect(() => {
     if (visible && path) {
       const item = _.get(mjmlJson, path);
-      if (item && item.attributes && item.attributes[ATTRIBUTE]) {
+      if (item?.attributes?.[ATTRIBUTE]) {
         setValue(item.attributes[ATTRIBUTE]);
       }
     }
-  }, [visible, path]);
+  }, [visible, path, mjmlJson]);
 
   useEffect(() => {
-    value && UpdateValue({ visible, path, mjmlJson, setMjmlJson, value, attribute: ATTRIBUTE });
-  }, [value]);
+    if (value) {
+      UpdateValue({
+        visible,
+        path,
+        mjmlJson,
+        setMjmlJson,
+        value,
+        attribute: ATTRIBUTE,
+      });
+    }
+  }, [value, visible, path, mjmlJson, setMjmlJson]);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const value = e.currentTarget.value;
-    logger.log('inner padding value', value);
+    const newValue = e.currentTarget.value;
+    logger.log('inner padding value', newValue);
     if (path && visible) {
-      setValue(value);
+      setValue(newValue);
     }
   };
 
-  return visible ? (
-    <Form.Item label="Inner Padding :">
-      <Input onChange={handleChange} type="text" value={value} />
-    </Form.Item>
-  ) : null;
+  if (!visible) return null;
+
+  return (
+    <Box>
+      <Typography variant="subtitle2" gutterBottom>
+        Inner Padding :
+      </Typography>
+      <TextField
+        fullWidth
+        size="small"
+        type="text"
+        value={value || ''}
+        onChange={handleChange}
+      />
+    </Box>
+  );
 };
 
 export { InnerPadding };
